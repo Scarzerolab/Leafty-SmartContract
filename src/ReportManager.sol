481aa6ee086f2fcc92d601f388538d2d7918ae75
trahwidhi <trahwidhi.oxtan17@gmail.com>
2025-07-30 23:19:36 +0800

    test import lib

--- src/ReportManager.sol
+++ src/ReportManager.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// import "https://github.com/bokkypoobah/BokkyPooBahsDateTimeLibrary/blob/master/contracts/BokkyPooBahsDateTimeLibrary.sol";
+
 contract ReportManager {
     enum WeatherCondition {
         SUNNY,
